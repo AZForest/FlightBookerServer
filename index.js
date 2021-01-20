212cc1d4559bcd9fe6dc5360ca0774c4d88a6d2f
AZForest <61096655+AZForest@users.noreply.github.com>
2021-01-20 08:22:53 -0800

    fix3

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = Process.env.PORT || 3001;
+const port = process.env.PORT;
 
 //BOdy Parser Middleware
 app.use(express.json());
